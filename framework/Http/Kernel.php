8e3ba2c3a2227e46e53c4239013db91f59cf6698
Gary Clarke <info@garyclarke.tech>
2023-01-19 20:48:15 +0000

    Add new routes and ahppy path handling

--- framework/Http/Kernel.php
+++ framework/Http/Kernel.php
@@ -2,12 +2,40 @@
 
 namespace GaryClarke\Framework\Http;
 
+use FastRoute\Dispatcher;
+use FastRoute\RouteCollector;
+use function FastRoute\simpleDispatcher;
+
 class Kernel
 {
     public function handle(Request $request): Response
     {
-        $content = '<h1>Hello World</h1>';
 
-        return new Response($content);
+
+        // Create a dispatcher
+        $dispatcher = simpleDispatcher(function (RouteCollector $routeCollector) {
+            $routeCollector->addRoute('GET', '/', function() {
+                $content = '<h1>Hello World</h1>';
+
+                return new Response($content);
+            });
+
+            $routeCollector->addRoute('GET', '/posts/{id:\d+}', function($routeParams) {
+                $content = "<h1>This is Post {$routeParams['id']}</h1>";
+
+                return new Response($content);
+            });
+        });
+
+        // Dispatch a URI, to obtain the route info
+        $routeInfo = $dispatcher->dispatch(
+            $request->server['REQUEST_METHOD'],
+            $request->server['REQUEST_URI'],
+        );
+
+        [$status, $handler, $vars] = $routeInfo;
+
+        // Call the handler, provided by the route info, in order to create a Response
+        return $handler($vars);
     }
 }
