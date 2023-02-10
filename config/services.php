ae6a5060e02594a81b7800d024b6312538795761
Gary Clarke <info@garyclarke.tech>
2023-02-10 19:32:36 +0000

    Add env vars as container params

--- config/services.php
+++ config/services.php
@@ -1,12 +1,15 @@
 <?php
 
+$dotenv = new \Symfony\Component\Dotenv\Dotenv();
+$dotenv->load(dirname(__DIR__) . '/.env');
+
 $container = new \League\Container\Container();
 
 $container->delegate(new \League\Container\ReflectionContainer(true));
 
 # parameters for application config
 $routes = include BASE_PATH . '/routes/web.php';
-$appEnv = 'prod';
+$appEnv = $_SERVER['APP_ENV'];
 
 $container->add('APP_ENV', new \League\Container\Argument\Literal\StringArgument($appEnv));
 
