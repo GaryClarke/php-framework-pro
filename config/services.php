968f11256daaf479cedcfe53e0906d1d1bd22b36
Gary Clarke <info@garyclarke.tech>
2023-02-12 14:12:52 +0000

    Add twig templating engine

--- config/services.php
+++ config/services.php
@@ -1,7 +1,7 @@
 <?php
 
 $dotenv = new \Symfony\Component\Dotenv\Dotenv();
-$dotenv->load(dirname(__DIR__) . '/.env');
+$dotenv->load(BASE_PATH . '/.env');
 
 $container = new \League\Container\Container();
 
@@ -10,6 +10,7 @@ $container->delegate(new \League\Container\ReflectionContainer(true));
 # parameters for application config
 $routes = include BASE_PATH . '/routes/web.php';
 $appEnv = $_SERVER['APP_ENV'];
+$templatesPath = BASE_PATH . '/templates';
 
 $container->add('APP_ENV', new \League\Container\Argument\Literal\StringArgument($appEnv));
 
@@ -29,6 +30,12 @@ $container->extend(GaryClarke\Framework\Routing\RouterInterface::class)
 $container->add(GaryClarke\Framework\Http\Kernel::class)
     ->addArgument(GaryClarke\Framework\Routing\RouterInterface::class)
     ->addArgument($container);
+
+$container->addShared('filesystem-loader', \Twig\Loader\FilesystemLoader::class)
+    ->addArgument(new \League\Container\Argument\Literal\StringArgument($templatesPath));
+
+$container->addShared(\Twig\Environment::class)
+    ->addArgument('filesystem-loader');
 
 return $container;
 
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,15 +4,18 @@ namespace App\Controller;
 
 use App\Widget;
 use GaryClarke\Framework\Http\Response;
+use Twig\Environment;
 
 class HomeController
 {
-    public function __construct(private Widget $widget)
+    public function __construct(private Widget $widget, private Environment $twig)
     {
     }
 
     public function index(): Response
     {
+        dd($this->twig);
+
         $content = "<h1>Hello {$this->widget->name}</h1>";
 
         return new Response($content);
