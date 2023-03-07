e33a6474a25cd632003a7cfb6b48cabf72760e00
Gary Clarke <info@garyclarke.tech>
2023-03-07 19:54:25 +0000

    Add parseOptions

--- framework/src/Console/Application.php
+++ framework/src/Console/Application.php
@@ -25,11 +25,37 @@ class Application
         $command = $this->container->get($commandName);
 
         // Parse variables to obtain options and args
+        $args = array_slice($argv, 2);
+
+        $options = $this->parseOptions($args);
 
         // Execute the command, returning the status code
-        $status = $command->execute();
+        $status = $command->execute($options);
 
         // Return the status code
         return $status;
     }
+
+    private function parseOptions(array $args): array
+    {
+        $options = [];
+
+        foreach ($args as $arg) {
+            if (str_starts_with($arg, '--')) {
+                // This is an option
+                $option = explode('=', substr($arg, 2));
+                $options[$option[0]] = $option[1] ?? true;
+
+            }
+        }
+
+        return $options;
+    }
+
+
+
+
+
+
+
 }
