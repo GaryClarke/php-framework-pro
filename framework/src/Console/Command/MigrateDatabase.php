4540eccd2edf0da6a6d96b8c0f83ce884d6c909a
Gary Clarke <info@garyclarke.tech>
2023-03-08 20:28:39 +0000

    Comment db migration steps

--- framework/src/Console/Command/MigrateDatabase.php
+++ framework/src/Console/Command/MigrateDatabase.php
@@ -8,7 +8,19 @@ class MigrateDatabase implements CommandInterface
 
     public function execute(array $params = []): int
     {
-        echo 'Executing MigrateDatabase command' . PHP_EOL;
+        // Create a migrations table SQL if table not already in existence
+
+        // Get $appliedMigrations which are already in the database.migrations table
+
+        // Get the $migrationFiles from the migrations folder
+
+        // Get the migrations to apply. i.e. they are in $migrationFiles but not in $appliedMigrations
+
+        // Create SQL for any migrations which have not been run ..i.e. which are not in the database
+
+        // Add migration to database
+
+        // Execute the SQL query
 
         return 0;
     }
