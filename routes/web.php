ac16e3ee531f31c6f3d6852d37175e766f5ecc85
Gary Clarke <info@garyclarke.tech>
2023-04-07 18:42:37 +0100

    Create register form and routing

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ return [
     ['GET', '/posts/{id:\d+}', [\App\Controller\PostsController::class, 'show']],
     ['GET', '/posts', [\App\Controller\PostsController::class, 'create']],
     ['POST', '/posts', [\App\Controller\PostsController::class, 'store']],
+    ['GET', '/register', [\App\Controller\RegistrationController::class, 'index']],
     ['GET', '/hello/{name:.+}', function(string $name) {
         return new Response("Hello $name");
     }]
