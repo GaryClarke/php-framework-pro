1f60aef1f1ffe41ff3a33b4e7b0910385ff6861e
Gary Clarke <info@garyclarke.tech>
2023-04-15 17:12:02 +0100

    Add guest middleware

---
+++ framework/src/Http/Middleware/Guest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace GaryClarke\Framework\Http\Middleware;
+
+use GaryClarke\Framework\Authentication\SessionAuthentication;
+use GaryClarke\Framework\Http\RedirectResponse;
+use GaryClarke\Framework\Http\Request;
+use GaryClarke\Framework\Http\Response;
+use GaryClarke\Framework\Session\SessionInterface;
+
+class Guest implements MiddlewareInterface
+{
+    public function __construct(private SessionInterface $session)
+    {
+    }
+
+    public function process(Request $request, RequestHandlerInterface $requestHandler): Response
+    {
+        $this->session->start();
+
+        if ($this->session->has(SessionAuthentication::AUTH_KEY)) {
+            return new RedirectResponse('/dashboard');
+        }
+
+        return $requestHandler->handle($request);
+    }
+}
+
+
+
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,19 @@ return [
     ['GET', '/posts/{id:\d+}', [\App\Controller\PostsController::class, 'show']],
     ['GET', '/posts', [\App\Controller\PostsController::class, 'create']],
     ['POST', '/posts', [\App\Controller\PostsController::class, 'store']],
-    ['GET', '/register', [\App\Controller\RegistrationController::class, 'index']],
+    ['GET', '/register', [\App\Controller\RegistrationController::class, 'index',
+        [
+            \GaryClarke\Framework\Http\Middleware\Guest::class
+        ]
+    ]
+    ],
     ['POST', '/register', [\App\Controller\RegistrationController::class, 'register']],
-    ['GET', '/login', [\App\Controller\LoginController::class, 'index']],
+    ['GET', '/login', [\App\Controller\LoginController::class, 'index',
+        [
+            \GaryClarke\Framework\Http\Middleware\Guest::class
+        ]
+    ]
+    ],
     ['POST', '/login', [\App\Controller\LoginController::class, 'login']],
     ['GET', '/dashboard', [\App\Controller\DashboardController::class, 'index',
         [
